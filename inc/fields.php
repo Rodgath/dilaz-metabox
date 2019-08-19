9371ca13654a1ebba212d37adb79e58b49a19add
Rodgath <rodgath@gmail.com>
2019-08-19 20:22:11 +0300

    Update fields.php

--- inc/fields.php
+++ inc/fields.php
@@ -4,13 +4,13 @@
 || Dilaz Metabox Fields
 || --------------------------------------------------------------------------------------------
 ||
-|| @package		Dilaz Metabox
-|| @subpackage	Fields
-|| @since		Dilaz Metabox 1.0
-|| @author		Rodgath, https://github.com/Rodgath
-|| @copyright	Copyright (C) 2017, Rodgath LTD
-|| @link		https://github.com/Rodgath/Dilaz-Metabox-Plugin
-|| @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+|| @package    Dilaz Metabox
+|| @subpackage Fields
+|| @since      Dilaz Metabox 1.0
+|| @author     Rodgath, https://github.com/Rodgath
+|| @copyright  Copyright (C) 2017, Rodgath LTD
+|| @link       https://github.com/Rodgath/Dilaz-Metabox-Plugin
+|| @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 || 
 */ 
 
