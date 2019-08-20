db5775698604e410353783256ed4e5356f6d54b1
Rodgath <rodgath@gmail.com>
2019-08-20 06:17:16 +0300

    Update fields.php

--- inc/fields.php
+++ inc/fields.php
@@ -1149,8 +1149,8 @@ if (!class_exists('DilazMetaboxFields')) {
 			
 			$show_thumb         = isset($args['show_thumb']) && $args['show_thumb'] == FALSE ? 'false' : 'true';
 			$data_file_thumb    = 'data-file-thumb="'. $show_thumb .'"';
-			$is_file_multiple   = isset($args['multiple']) && $args['multiple'] == TRUE ? TRUE : FALSE;			
-			$data_file_multiple = (isset($args['multiple']) && $args['multiple'] == true) ? 'data-file-multiple="true"' : '';
+			$is_file_multiple   = isset($args['multiple']) && $args['multiple'] == TRUE ? TRUE : FALSE;				
+			$data_file_multiple = $is_file_multiple ? 'data-file-multiple="true"' : '';
 			$file_type          = (isset($args['file_type']) && $args['file_type'] != '') ? strtolower($args['file_type']) : 'image';
 			$data_file_type     = $file_type != '' ? 'data-file-type="'. $file_type .'"' : 'data-file-type="image"';
 			$data_file_specific = (isset($args['file_specific']) && $args['file_specific'] == true) ? 'data-file-specific="true"' : '';
