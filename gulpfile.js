d591a25a6600c2bac21a279109e9faa565b06499
Rodgath <25994782+Rodgath@users.noreply.github.com>
2025-02-02 12:41:43 +0300

    Update gulp to watch SCSS tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, series, parallel, watch, task } = require('gulp');
+const { src, dest, series, watch, task } = require('gulp');
 const dartSass = require('sass');    
 const gulpDartSass = require('gulp-dart-sass');
 const concat = require('gulp-concat');
@@ -6,12 +6,17 @@ const cleanCSS = require('gulp-clean-css');
 
 /* Combined CSS job */
 const cssCombined = () => {
-  return src(['assets/css/metabox.scss']) // Using 'src' directly from destructured gulp
+  return src(['assets/css/*.scss']) // Using 'src' directly from destructured gulp
     .pipe(gulpDartSass(dartSass).on('error', gulpDartSass.logError))
     .pipe(concat('metabox.min.css'))
     .pipe(cleanCSS())
     .pipe(dest('assets/css'));
 };
 
-// Default task
-task('default', series(cssCombined));
+/* Watch Task */
+const watchFiles = () => {
+  watch('assets/css/**/*.scss', cssCombined); // Watches all SCSS files in the directory and subdirectories
+};
+
+// Default task: Run cssCombined first, then watch for changes
+task('default', series(cssCombined, watchFiles));
